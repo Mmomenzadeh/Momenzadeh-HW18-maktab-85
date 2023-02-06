1a4319a36f787bf88b2b02b9de76d829e68b900d
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-02-06 15:50:04 +0330

    finish question-1 functionality

--- src/Components/Question-1/index.jsx
+++ src/Components/Question-1/index.jsx
@@ -1,28 +1,46 @@
-import { useState } from "react"
-import "./Question-1.css"
+import { useState } from "react";
+import "./Question-1.css";
 
-const Question1 = ()=>{
+const Question1 = () => {
+  const [colors, setColors] = useState([]);
 
-    const [colors , setColors] = useState([])
+  const renderColorHandler = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const generateColor = {
+      red,
+      green,
+      blue,
+    };
 
-    const renderColorHandler=()=>{}
-    return(
-        <div className="Question1">
-            <div className="generate-color">
-                <button onClick={renderColorHandler}>Add Color</button>
-                <div className="color-container">
-
-                <div className="color">
-                        <div className="background-color"></div>
-                        <p className="rgb-color">rgb(100,100,100)</p>
-                    </div>
-
-                
-
-                </div>
-            </div>
+    setColors([...colors, generateColor]);
+  };
+  return (
+    <div className="Question1">
+      <div className="generate-color">
+        <button onClick={renderColorHandler}>Add Color</button>
+        <div className="color-container">
+          {colors.map((color, index) => {
+            return (
+              <div className="color" key={index}>
+                <div
+                  className="background-color"
+                  style={{
+                    backgroundColor: ` rgb(${color.red} ,${color.green} ,${color.blue})`,
+                  }}
+                ></div>
+                <p className="rgb-color">
+                  {" "}
+                  rgb{`(${color.red} ,${color.green} ,${color.blue})`}
+                </p>
+              </div>
+            );
+          })}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Question1
+export default Question1;
