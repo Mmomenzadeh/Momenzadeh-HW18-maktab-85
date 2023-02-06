a23085cecaa970f4e9186c8c6c67559872862b07
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-02-06 14:22:25 +0330

    finish functionality question-3

--- src/Components/Question-3/index.jsx
+++ src/Components/Question-3/index.jsx
@@ -1,30 +1,79 @@
-import "./Question-3.css"
+import { useState } from "react";
+import "./Question-3.css";
 
-const Question3 =()=>{
-
-    return(
-        <div className="Auth-form">
-            <form style={{padding : "20px"}}>
-                <h2>Create account</h2>
-                <p>Already have an account ? <a>Sign in</a></p>
-                <input type="text" placeholder="Username"/>
-                <div className="field-NF">
-                <input type="text" placeholder="Last name "/>
-                <input type="text" placeholder="First Name "/>
-                </div>
-                <input type="email" placeholder="E-mail"/>
-
-                <button>Sign up</button>
-
-                <div className="field-ChL">
-                    <input type="checkbox" />
-                    <label> i have read and agree to <a>Terms of Service</a> </label>
-                </div>
+const Question3 = () => {
+  const newUser = {
+    id: Math.floor(Math.random() * 1000),
+    name: "",
+    fmaily: "",
+    email: "",
+    userName: "",
+  };
+  const [inputValue, setInputValue] = useState(newUser);
 
 
-            </form>
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(inputValue);
+  };
+  return (
+    <div className="Auth-form">
+      <form style={{ padding: "20px" }} onSubmit={onSubmitHandler}>
+        <h2>Create account</h2>
+        <p>
+          Already have an account ? <a>Sign in</a>
+        </p>
+        <input
+          type="text"
+          placeholder="Username"
+          value={inputValue.userName}
+          onChange={(e) =>
+            setInputValue({ ...inputValue, userName: e.target.value })
+          }
+          required 
+        />
+        <div className="field-NF">
+          <input
+            type="text"
+            placeholder="Last name "
+            value={inputValue.fmaily}
+            onChange={(e) =>
+              setInputValue({ ...inputValue, fmaily: e.target.value })
+            }
+            required 
+          />
+          <input
+            type="text"
+            placeholder="First Name "
+            value={inputValue.name}
+            onChange={(e) =>
+              setInputValue({ ...inputValue, name: e.target.value })
+            }
+            required 
+          />
         </div>
-    )
-}
+        <input
+          type="email"
+          placeholder="E-mail"
+          value={inputValue.email}
+          onChange={(e) =>
+            setInputValue({ ...inputValue, email: e.target.value })
+          }
+          required 
+        />
 
-export default Question3
+        <button>Sign up</button>
+
+        <div className="field-ChL">
+          <input type="checkbox" />
+          <label>
+            i have read and agree to <a>Terms of Service</a>{" "}
+          </label>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Question3;
